b400ad085b4a29c9fd32526cbe47a78ae88cccba
Guilherme Gregorio <guilhermegregoriodossantos@gmail.com>
2025-04-02 21:17:27 -0300

    feat: adição filas mensageria

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,5 +1,7 @@
 import User from "../models/UserModel.js";
 import createHttpError from "http-errors";
+import Publisher from "../utils/Publisher.js";
+import subscriber from "../utils/Subscrriber.js";
 
 export const getUsers = async (req, res, next) => {
   try {
@@ -8,6 +10,8 @@ export const getUsers = async (req, res, next) => {
     if (!response.length) {
       return next(createHttpError(404, "Nenhum usuário encontrado"));
     }
+    await Publisher("user_events", { type: "getUser", data: response });
+    await subscriber();
 
     res.status(200).json(response);
   } catch (error) {
@@ -26,7 +30,8 @@ export const getUserById = async (req, res, next) => {
     if (!response) {
       return next(createHttpError(404, "Nenhum usuário encontrado"));
     }
-
+    await Publisher("user_events", { type: "getUserById", data: response });
+    await subscriber();
     res.status(200).json(response);
   } catch (error) {
     next(createHttpError(500, "Erro interno ao buscar usuário"));
@@ -38,8 +43,11 @@ export const CreateUser = async (req, res, next) => {
     const affectedRows = await User.create(req.body);
 
     if (affectedRows[0] === 0) {
-      return next(createHttpError(404, "Usuário nçao criado tente novamente"));
+      return next(createHttpError(404, "Usuário não criado tente novamente"));
     }
+    await Publisher("user_events", { type: "CreateUser", data: affectedRows });
+    await subscriber();
+
     res.status(201).json({ message: "User Criado" });
   } catch (error) {
     next(createHttpError(500, "Erro interno ao Criar usuários"));
@@ -57,6 +65,8 @@ export const UpdateUser = async (req, res, next) => {
     if (affectedRows[0] === 0) {
       return next(createHttpError(404, "Id não encontrado"));
     }
+    await Publisher("user_events", { type: "UpdateUser", data: affectedRows });
+    await subscriber();
 
     res.status(200).json({ message: "User atualizado" });
   } catch (error) {
@@ -76,6 +86,9 @@ export const DeleteUser = async (req, res, next) => {
     if (affectedRows === 0) {
       return next(createHttpError(404, "Id não encontrado"));
     }
+
+    await Publisher("user_events", { type: "DeleteUser", data: affectedRows });
+    await subscriber();
 
     res.status(200).json({ message: "User Deletado" });
   } catch (error) {
